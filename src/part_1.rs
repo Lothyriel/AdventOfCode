6d6faa5d432afd8693900f7e3451cc7d9e48913c
fastjonh <fastjonh@gmail.com>
2023-12-01 22:00:05 -0300

    part 1 finished

---
+++ src/part_1.rs
@@ -0,0 +1,81 @@
+pub fn get_calibration_value(input: &str) -> Result<usize, Error> {
+    input.lines().map(get_calibration_line).sum()
+}
+
+fn get_calibration_line(input: &str) -> Result<usize, Error> {
+    let mut numbers = input.bytes().filter(|b| b.is_ascii_digit());
+
+    let first = to_digit(numbers.next())?;
+
+    let last = to_digit(numbers.next_back()).unwrap_or(first);
+
+    Ok(first * 10 + last)
+}
+
+fn to_digit(byte: Option<u8>) -> Result<usize, Error> {
+    let byte = byte.ok_or(Error::NumberNotFound)?;
+
+    match byte {
+        b'0' => Ok(0),
+        b'1' => Ok(1),
+        b'2' => Ok(2),
+        b'3' => Ok(3),
+        b'4' => Ok(4),
+        b'5' => Ok(5),
+        b'6' => Ok(6),
+        b'7' => Ok(7),
+        b'8' => Ok(8),
+        b'9' => Ok(9),
+        _ => Err(Error::Parsing),
+    }
+}
+
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum Error {
+    #[error("Couldn't find a number in the input line")]
+    NumberNotFound,
+    #[error("Couldn't convert to a digit representation")]
+    Parsing,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_lines() {
+        let input = [
+            ("1abc2", 12),
+            ("pqr3stu8vwx", 38),
+            ("a1b2c3d4e5f", 15),
+            ("treb7uchet", 77),
+            ("9s", 99),
+        ];
+
+        for (i, r) in input {
+            let value = get_calibration_line(i);
+            assert_eq!(value, Ok(r));
+        }
+    }
+
+    #[test]
+    fn example() {
+        let input = r#"1abc2
+                       pqr3stu8vwx
+                       a1b2c3d4e5f
+                       treb7uchet"#;
+
+        let value = get_calibration_value(input);
+
+        assert_eq!(value, Ok(142));
+    }
+
+    #[test]
+    fn puzzle() {
+        let input = include_str!("input");
+
+        let value = get_calibration_value(input);
+
+        assert_eq!(value, Ok(54450));
+    }
+}
