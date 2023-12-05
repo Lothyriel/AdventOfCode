728229a1e4cdc9afb9f3a197a6f1c2929a94f4bf
fastjonh <fastjonh@gmail.com>
2023-12-05 03:30:18 -0300

    moving parser to lib

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -1,1 +1,98 @@
+use std::collections::VecDeque;
+
 pub mod part_1;
+pub mod part_2;
+
+fn get_tokens(input: &str) -> Vec<((usize, usize), Token)> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(i, l)| parse_line(i, l))
+        .collect()
+}
+
+fn parse_line(line: usize, input: &str) -> Vec<(Position, Token)> {
+    let mut input: VecDeque<_> = input.bytes().enumerate().collect();
+    let mut tokens = Vec::new();
+
+    while !input.is_empty() {
+        if let Some(t) = parse(line, &mut input) {
+            tokens.push(t);
+        }
+    }
+
+    tokens
+}
+
+fn parse(line: usize, input: &mut VecDeque<(usize, u8)>) -> Option<(Position, Token)> {
+    let (_, b) = input.front()?;
+
+    match *b {
+        b'0'..=b'9' => {
+            let (c, n) = parse_number(input);
+            Some(((line, c), n))
+        }
+        b'.' => {
+            input.pop_front()?;
+            None
+        }
+        _ => {
+            let (i, b) = input.pop_front()?;
+            let position = (line, i);
+            Some((position, Token::Symbol(b)))
+        }
+    }
+}
+
+fn parse_number(input: &mut VecDeque<(usize, u8)>) -> (usize, Token) {
+    let mut digits = Vec::new();
+
+    while let Some(t) = input.front() {
+        if t.1.is_ascii_digit() {
+            let d = input.pop_front().expect("Should have more digits");
+            digits.push(d);
+        } else {
+            break;
+        }
+    }
+
+    let n = String::from_utf8(digits.iter().map(|t| t.1).collect()).expect("Should be valid UTF-8");
+
+    let column = digits.first().expect("Should have a first digit").0;
+
+    (column, Token::Number(n))
+}
+
+type Position = (usize, usize);
+
+#[derive(PartialEq, Debug)]
+enum Token {
+    Number(String),
+    Symbol(u8),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parsing_first_line() {
+        let result = parse_line(0, "467..114..");
+
+        let expected = vec![
+            ((0, 0), Token::Number("467".to_string())),
+            ((0, 5), Token::Number("114".to_string())),
+        ];
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn parsing_second_line() {
+        let result = parse_line(1, "...*......");
+
+        let expected = vec![((1, 3), Token::Symbol(b'*'))];
+
+        assert_eq!(result, expected);
+    }
+}
--- day_03/src/part_1.rs
+++ day_03/src/part_1.rs
@@ -1,23 +1,19 @@
-use std::collections::VecDeque;
+use crate::{get_tokens, Position, Token};
 
 pub fn get_schematic_value(input: &str) -> usize {
-    let tokens: Vec<_> = input
-        .lines()
-        .enumerate()
-        .flat_map(|(i, l)| parse_line(i, l))
-        .collect();
+    let tokens = get_tokens(input);
 
     let symbol_positions: Vec<_> = tokens
         .iter()
         .filter_map(|t| match t.1 {
             Token::Symbol(_) => Some(t.0),
-            Token::Number(_) => None,
+            _ => None,
         })
         .collect();
 
     let numbers = tokens.into_iter().filter_map(|t| match t.1 {
         Token::Number(n) => Some((t.0, n)),
-        Token::Symbol(_) => None,
+        _ => None,
     });
 
     numbers
@@ -33,6 +29,15 @@ fn valid_parts(symbols: &[Position], number: &(Position, String)) -> bool {
         .any(|x| x)
 }
 
+fn valid(position: Position, n: usize, symbol: &(usize, usize)) -> bool {
+    let (line, col) = position;
+
+    BORDER_OFFSETS
+        .iter()
+        .map(|r| (line as i32 + r.0, r.1 + col as i32 + n as i32))
+        .any(|b| (b.0, b.1) == (symbol.0 as i32, symbol.1 as i32))
+}
+
 const BORDER_OFFSETS: [(i32, i32); 9] = [
     (-1, -1),
     (-1, 0),
@@ -45,99 +50,9 @@ const BORDER_OFFSETS: [(i32, i32); 9] = [
     (1, 1),
 ];
 
-fn valid(position: Position, n: usize, symbol: &(usize, usize)) -> bool {
-    let (line, col) = position;
-
-    BORDER_OFFSETS
-        .iter()
-        .map(|r| (line as i32 + r.0, r.1 + col as i32 + n as i32))
-        .any(|b| (b.0, b.1) == (symbol.0 as i32, symbol.1 as i32))
-}
-
-fn parse_line(line: usize, input: &str) -> Vec<(Position, Token)> {
-    let mut input: VecDeque<_> = input.bytes().enumerate().collect();
-    let mut tokens = Vec::new();
-
-    while !input.is_empty() {
-        if let Some(t) = parse(line, &mut input) {
-            tokens.push(t);
-        }
-    }
-
-    tokens
-}
-
-fn parse(line: usize, input: &mut VecDeque<(usize, u8)>) -> Option<(Position, Token)> {
-    let (_, b) = input.front()?;
-
-    match *b {
-        b'0'..=b'9' => {
-            let (c, n) = parse_number(input);
-            Some(((line, c), n))
-        }
-        b'.' => {
-            input.pop_front()?;
-            None
-        }
-        _ => {
-            let (i, b) = input.pop_front()?;
-            let position = (line, i);
-            Some((position, Token::Symbol(b)))
-        }
-    }
-}
-
-fn parse_number(input: &mut VecDeque<(usize, u8)>) -> (usize, Token) {
-    let mut digits = Vec::new();
-
-    while let Some(t) = input.front() {
-        if t.1.is_ascii_digit() {
-            let d = input.pop_front().expect("Should have more digits");
-            digits.push(d);
-        } else {
-            break;
-        }
-    }
-
-    let n = String::from_utf8(digits.iter().map(|t| t.1).collect()).expect("Should be valid UTF-8");
-
-    let column = digits.first().expect("Should have a first digit").0;
-
-    (column, Token::Number(n))
-}
-
-type Position = (usize, usize);
-
-#[derive(PartialEq, Debug)]
-enum Token {
-    Number(String),
-    Symbol(u8),
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn parsing_first_line() {
-        let result = parse_line(0, "467..114..");
-
-        let expected = vec![
-            ((0, 0), Token::Number("467".to_string())),
-            ((0, 5), Token::Number("114".to_string())),
-        ];
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn parsing_second_line() {
-        let result = parse_line(1, "...*......");
-
-        let expected = vec![((1, 3), Token::Symbol(b'*'))];
-
-        assert_eq!(result, expected);
-    }
 
     #[test]
     fn example() {
