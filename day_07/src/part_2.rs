e2f56528df0716d1e240ad1b1a81569459779213
fastjonh <fastjonh@gmail.com>
2023-12-09 02:38:58 -0300

    making change_jester recursive

--- day_07/src/part_2.rs
+++ day_07/src/part_2.rs
@@ -1,9 +1,14 @@
 use std::cmp::Ordering;
 
+use crate::{Card, Hand};
+
 pub fn get_total_winnings(input: &str) -> usize {
     let mut games: Vec<_> = crate::parse_games(input).collect();
 
-    change_jester(&mut games);
+    games
+        .iter_mut()
+        .map(|g| (g.cards.iter().filter(|c| matches!(c, Card::J)).count(), g))
+        .for_each(|(c, g)| change_jester(g, c));
 
     games.sort_by(|g1, g2| {
         let ord = g1.hand.value().cmp(&g2.hand.value());
@@ -17,8 +22,21 @@ pub fn get_total_winnings(input: &str) -> usize {
     games.iter().enumerate().map(|(i, g)| g.bid * (i + 1)).sum()
 }
 
-fn change_jester(games: &mut [crate::Game]) {
-    for game in games {}
+fn change_jester(game: &mut crate::Game, count: usize) {
+    if count == 0 {
+        return;
+    }
+
+    game.hand = match game.hand {
+        Hand::High(c) => Hand::One(c),
+        Hand::One(c) => Hand::Two(c, *game.cards.first().expect("Should have card")),
+        Hand::Two(c1, c2) => Hand::Full(c1, c2),
+        Hand::Three(c) => Hand::Four(c),
+        Hand::Full(c, _) => Hand::Four(c),
+        Hand::Four(c) | Hand::Five(c) => Hand::Five(c),
+    };
+
+    change_jester(game, count - 1)
 }
 
 #[cfg(test)]
