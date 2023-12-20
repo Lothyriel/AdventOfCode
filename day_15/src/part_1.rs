c97eb0d4f0ceb3b92f6f510922fb60992331442d
fastjonh <fastjonh@gmail.com>
2023-12-19 23:14:02 -0300

    solving day 15 part one

---
+++ day_15/src/part_1.rs
@@ -0,0 +1,35 @@
+pub fn get_hash_sum(input: &str) -> usize {
+    input.trim().split(',').map(hash).sum()
+}
+
+fn hash(input: &str) -> usize {
+    input
+        .bytes()
+        .fold(0, |acc, b| ((acc + b as usize) * 17) % 256)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hash() {
+        let result = hash("HASH");
+
+        assert_eq!(result, 52);
+    }
+
+    #[test]
+    fn example() {
+        let result = get_hash_sum("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7");
+
+        assert_eq!(result, 1320);
+    }
+
+    #[test]
+    fn puzzle() {
+        let result = get_hash_sum(include_str!("input"));
+
+        assert_eq!(result, 494980);
+    }
+}
